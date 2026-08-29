021fea44916580d4e46fe6695d51c24012d857bf
Kabelo12-pixel <mpekoakabelo00@gmail.com>
2026-08-28 14:55:15 -1200

    feat(inventory): create Inventory class with ArrayList collection

--- src/main/java/com/mycompany/inventorymanagementsystem/Inventory.java
+++ src/main/java/com/mycompany/inventorymanagementsystem/Inventory.java
@@ -1,17 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.inventorymanagementsystem;
+import java.util.ArrayList;
 
 /**
  *
  * @author LENOVO
  */
-public class InventoryManagementSystem {
-
-    public static void main(String[] args) {
-        Item i = new Item();
-        Inventory in = new Inventory();
-    }
+public class Inventory {
+    
+    ArrayList<Item> = new Item(); 
+    
 }
